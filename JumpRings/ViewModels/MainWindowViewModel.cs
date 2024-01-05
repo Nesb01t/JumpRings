4de19bd39c61bf304c68b8bd8021c6f551d257f3
Nesb01t <nesb01t@163.com>
2024-01-05 12:34:48 +0800

    first cmt

---
+++ JumpRings/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JumpRings.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string HelloDesc => "👋Hello there, This is JumpRings";
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ JumpRings/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace JumpRings.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
